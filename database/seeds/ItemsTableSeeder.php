0efcd1a5254462ae0abe1a150aaf07b7ad61df95
Jon Wurtzler <jwurtzler@myriadmobile.com>
2016-07-07 02:03:52 -0500

    Added updated item seeds and documentation.

--- database/seeds/ItemsTableSeeder.php
+++ database/seeds/ItemsTableSeeder.php
@@ -26,23 +26,30 @@ class ItemsTableSeeder extends Seeder
     public function run()
     {
       $this->item->create([
-        'name'        => 'Moogle',
-        'description' => '',
-        'rarity'      => '2',
-        'case'        => ''
-      ]);
-
-      $this->item->create([
-        'name'        => 'Tanooki Suit',
-        'description' => '',
-        'rarity'      => '2',
-        'case'        => ''
-      ]);
-
-      $this->item->create([
         'name'        => 'Banana Peel',
         'description' => 'Person behind you 50% chance -100 points',
         'rarity'      => '1',
+        'case'        => 'Some'
+      ]);
+
+      $this->item->create([
+        'name'        => 'Biggs',
+        'description' => 'Red Wing soldier that bolsters your confidence.',
+        'rarity'      => '1',
+        'case'        => 'Some'
+      ]);
+
+      $this->item->create([
+        'name'        => 'Bo Jackson',
+        'description' => 'TOUCHDOWN!',
+        'rarity'      => '1',
+        'case'        => 'Some'
+      ]);
+
+      $this->item->create([
+        'name'        => 'Buffalo',
+        'description' => 'Absolutely not dying from dysentery today!',
+        'rarity'      => '2',
         'case'        => 'Some'
       ]);
 
@@ -54,9 +61,107 @@ class ItemsTableSeeder extends Seeder
       ]);
 
       $this->item->create([
+        'name'        => 'Crowbar',
+        'description' => 'Gordon Freeman\'s weapon of choice.',
+        'rarity'      => '1',
+        'case'        => 'Some'
+      ]);
+
+      $this->item->create([
+        'name'        => 'Da Da Da Da Daaa Da DAA da da',
+        'description' => 'The victory song that delivers a sweet reward.',
+        'rarity'      => '2',
+        'case'        => 'Some'
+      ]);
+
+      $this->item->create([
+        'name'        => 'Fire Flower',
+        'description' => 'Throw balls of actual fire. Literally.',
+        'rarity'      => '1',
+        'case'        => 'Some'
+      ]);
+
+      $this->item->create([
+        'name'        => 'Gold Ring',
+        'description' => 'Shiny ring that provides temporary protection from damage.',
+        'rarity'      => '2',
+        'case'        => 'Some'
+      ]);
+
+      $this->item->create([
+        'name'        => 'Green Shell',
+        'description' => 'Throw at opponent to deliver a good thump on the head.',
+        'rarity'      => '1',
+        'case'        => 'Some'
+      ]);
+
+      $this->item->create([
+        'name'        => 'Hard Knuckle',
+        'description' => 'Mega Man face punch.',
+        'rarity'      => '2',
+        'case'        => 'Some'
+      ]);
+
+      $this->item->create([
         'name'        => 'Leeroy Jenkins',
         'description' => 'Absolutely pathetic battle cry.',
         'rarity'      => '3',
+        'case'        => 'Some'
+      ]);
+
+      $this->item->create([
+        'name'        => 'Moogle',
+        'description' => '',
+        'rarity'      => '2',
+        'case'        => ''
+      ]);
+
+      $this->item->create([
+        'name'        => 'Mushroom',
+        'description' => 'Maybe edible mushroom.',
+        'rarity'      => '1',
+        'case'        => 'Some'
+      ]);
+
+      $this->item->create([
+        'name'        => 'Pizza',
+        'description' => '',
+        'rarity'      => '',
+        'case'        => ''
+      ]);
+
+      $this->item->create([
+        'name'        => 'Red Crystal',
+        'description' => 'You have the power of tornadoes! Take that Deborah Cliff.',
+        'rarity'      => '3',
+        'case'        => 'Some'
+      ]);
+
+      $this->item->create([
+        'name'        => 'Red Shell',
+        'description' => 'Toss ahead of you to unsuspecting victim.',
+        'rarity'      => '1',
+        'case'        => 'Some'
+      ]);
+
+      $this->item->create([
+        'name'        => 'Tanooki Suit',
+        'description' => 'Adorable suit that provides temporary protection from attacks.',
+        'rarity'      => '2',
+        'case'        => 'Some'
+      ]);
+
+      $this->item->create([
+        'name'        => 'UUDDLRLRBA',
+        'description' => 'Konami Code always gives a nice boost.',
+        'rarity'      => '1',
+        'case'        => 'Some'
+      ]);
+
+      $this->item->create([
+        'name'        => 'Wedge',
+        'description' => 'Red Wing soldier that bolsters your confidence.',
+        'rarity'      => '1',
         'case'        => 'Some'
       ]);
     }
--- readme.md
+++ readme.md
@@ -3,16 +3,117 @@
 - May only make one call to 'points' every second.  Any more will cause a negative effect to be applied.
 - May only use one item per minute.  Any more will cause a negative effect to be applied.
 
+## Items
+
+### Bo Jackson
+
+  - Description: TOUCHDOWN!
+  - Rarity: 1
+  - Effect: Tried on self, gained 7 points.
+  
+### Biggs
+
+  - Description: Red Wing soldier that bolsters your confidence.
+  - Rarity: 1
+  - Effect: Tried on self, gained 50 points.
+  
+### Fire Flower
+
+  - Description: Throw balls of actual fire. Literally.
+  - Rarity: 1
+  - Effect: ??
+  
+### Gold Ring
+
+  - Description: Shiny ring that provides temporary protection from damage.
+  - Rarity: 2
+  - Effect: Tried on self, gained 'Gold Ring' effect.
+
+### Pizza
+  
+  - Effect: Tried it on myself, I gained 0 points.
+
+### Moogle
+
+  - Description: 
+  - Rarity: 2
+  - Effect: Tried on self, gained effect 'Moogle'
+  
+### Red Shell
+  
+  - Description: Toss ahead of you to unsuspecting victim.
+  - Rarity: 1
+  - Effect: ?? Guessing hits the person ahead of you for dmg.
+  
+### Tanooki Suit
+
+  - Description:
+  - Rarity: 2
+  - Effect: Tried on self, gained effect 'Tanooki Suit'
+  
+### UUDDLRLRBA
+
+  - Description: Konami Code always gives a nice boost.
+  - Rarity: 1
+  - Effect: Tried on self, gained 30 points.
+  
+### Wedge
+
+  - Description: Red Wing soldier that bolsters your confidence.
+  - Rarity: 1
+  - Effect: Tried on self, gained 50 points.
+
 ## Effects
+
+### Gold Ring
+
+ - Duration: 3 min
+ - Effect: ?? - guessing protection from attacks.  One ring, so all attacks?
 
 ### Moogle
 
  - duration: 30min
- - rarity: 2
  - effect: may gain 1 or 3 points when gathering points
  
 ### Tanooki Suit
  
  - duration: 3 attacks
- - rarity: 2
  - effect: protection from attacks
+ 
+## Violations
+ 
+### Poison
+ 
+  - duration: instant
+  - effect: ??
+  
+### Shell Shock
+
+  - duration: 3 min
+  - effect: ??
+  
+### Sandstorm
+
+  - duration: instant
+  - effect: ??
+  
+### Petrify
+
+  - duration: 10 min
+  - effect: ??
+  
+### TKO
+  
+  - duration: 1 min
+  - effect: connection refused
+  
+### Quicksand
+  
+  - duration: 5 min
+  - effect: ??
+  
+### Slow
+
+  - duration: 15 min
+  - effect: half points
+  
